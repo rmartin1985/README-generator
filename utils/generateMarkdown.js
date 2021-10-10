3ffc36e1c72d0185efefca712f9f5923108a3a56
Rick Martin <rmartin1985@att.net>
2021-10-10 17:28:23 -0500

    fixed styling for sample readme badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    const renderBadge = `(https://img.shields.io/badge/License-${license}-blue.svg)`;
+    const renderBadge = `![](https://img.shields.io/badge/license-${license}-blue)`;
     if (license == "None") {
         return ""
     } else {
